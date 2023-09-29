cf0a6e4180e7c60d7bda280d2f9d066e6841abb4
wezbest <110793713+wezbest@users.noreply.github.com>
2023-09-29 18:01:05 +0000

    .

--- gwz/gopGITHUB.fish
+++ gwz/gopGITHUB.fish
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Delete the old version which is located in /usr/local/go"
-rm -rf /usr/local/go
+sudo rm -rf /usr/local/go
 echo "Get the latest binary from https://go.dev/dl/ a the time of writing it is 1.21 which will be downloaded "
 echo " 1.21.1 - https://go.dev/dl/go1.20.7.linux-amd64.tar.gz"
 echo ""
