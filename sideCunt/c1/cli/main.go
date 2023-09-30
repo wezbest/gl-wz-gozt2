0706b515fdc47177c598f6ffc157b81f15ac597a
wezbest <110793713+wezbest@users.noreply.github.com>
2023-09-30 12:18:21 +0000

    .

---
+++ sideCunt/c1/cli/main.go
@@ -0,0 +1,14 @@
+/*
+MMain Entry point for the program 
+*/
+
+package main 
+
+import (
+	f "fmt"
+)
+
+// This will have the main functions
+func main() {
+	f.Println("Hello World")
+}
---
+++ sideCunt/c1/lib/lip.go
@@ -0,0 +1,12 @@
+/*
+This will be settings for the lipgloss stuff  from this git
+https://github.com/charmbracelet/lipgloss
+*/
+
+package lib 
+
+import (
+	f "fmt"
+	lp "github.com/charmbracelet/lipgloss"
+)
+
