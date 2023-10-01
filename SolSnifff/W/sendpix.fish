d47636436d83915db2bbba8bf225035c4e98338c
wezbest <110793713+wezbest@users.noreply.github.com>
2023-10-01 12:41:31 +0000

    .

--- SolSnifff/W/sendpix.fish
+++ SolSnifff/W/sendpix.fish
@@ -9,6 +9,31 @@ set W2K 0xe97c3c7130b94bc6f64b98d8f1d195ab5e49dea477eed60934d217ce9cd58d27
 # This Section for setting ASCII Image 
 
 set wallet1text " 
+....                          ...',,,,'...;..':dM0l. ..,lWN:. . '      ox       
+'.......                      ....,,''.;.:;....;XMk;'...ckNMk;'..      dx       
+,'.... .    .....               ..',''..ll'  ..:k0Nl;...,;lkKWN0k      cd:;;;;;:
+oc::,'...........              ..''';' ;xl  ...ckoKN:;,;,.,,;ckXK      ;;       
+XWXOoc,'''...                 ...'.,c,,0d   ..'l0NNMdl:ll::''''xK      cc       
+ .lOXNN0dc;',.....  ...         .''lcoNd   ..,xKKoXMKOkdo:c,,c:0K      ll       
+     .:xKWWKkdc::;''.......  ....,:loX: . .dOKKd;.cMM0kddOXWWNNKx.     ;o       
+          ,d0NWKOdl:;,''''..,''''::O0,,okOKX,oc,'.xKMxOOMMWc...'.      :o       
+              .lOXNNNkxkdllc::;lk0XKKKk00;l:,'....K0Mx,c0XWNx.';       cd       
+                 cdXMXOO00KXO0K0xoo:co:;;,.. ..'.'kcMk .:ldKMXl,.......;o..     
+                .MNdOk;,.'Ol.....''...,::'',':ll,,dcNO   ':;oNN.       .,       
+                 KWllo::,lK. .... .':odlc::;;...'.:XNN. .:cc.,NK.      .,       
+                 dMX,c:,;'0: ..';dO00KKKK0OOOoxklc.;NM0xxkKdl.xM.      .'       
+  ..             .WNX0O,,,d0kO0KKOxll:,...;cdk0NXXO;0MWox;lOl,kMo      ..       
+                  0WXNNXXNXOxoccodkOk'. ......':dOWWWMNlkX;;xoOKW.     .lllooooo
+                 .cMWNNXOol:;,,,,,clk0O'.    ..'':lKNWX,kNN,.;ldM       .       
+                 .dMW0k:,;'.......'.,lxXO,     ..,,dKWl.dkWW;..dMddddddxc       
+               .oNM0l;'.....    ....',;lkKo     .''cKM'.,dlNX. cM:::;;:kK       
+.             cNWk:........         .,,;;d00,. ....lKM.ldxolKl.,M      dK       
+...         ,KMXl,.........          ..,.'l:Kd.....ONN,l.:d;xk.,MkOOOOOOO       
+.         .OMXx,'.....              . .'..'c.ll..,lOMk o..;dOd.xM      .d       
+        .xWXo;.......            .   ..... ,'.;:':OWK; l'..'OXNKdcccccclc       
+       oWNx:.....   .             .   ''....,..,;xWN'o,:c..''XW,                
+     'XWO:..... .   .                 .;.. .''.'cWN.  ,.c '. ,XW.              
+
  ██╗    ██╗  █████╗  ██╗      ██╗      ███████╗ ████████╗  ██╗    
  ██║    ██║ ██╔══██╗ ██║      ██║      ██╔════╝ ╚══██╔══╝ ███║    
  ██║ █╗ ██║ ███████║ ██║      ██║      █████╗      ██║    ╚██║    
@@ -25,6 +50,30 @@ set wallet1text "
 "
 
 set wallet2text " 
+W..,klo:c:cK0                                                ':. .      ...  .'M
+Mx.'l0dd'c;kK.                                            .  ,c.   . .        .M
+XlKXOldc.oN0O. ..  ..                                     ...c:.  .           .M
+Mx0Kx:kOOOo.........                          ...       .',;;l,,;cldxkkOOOOO000W
+NX0K0x;.                                                             ....''',,'K
+k,                                                     ..;,';;:cc::::;;;;,,'',,M
+O,;:cldxxdl:,.       .           .:dOKXKKKKKXXXXXKNWNNWXdddlc;'...............,M
+X........':lodxkkxxxkkO0k0Ok000KKX0kdc',;,',,,;:::dkXNd'''....................,M
+N.. ..   .       ....   .:lkkN0xl, ..',;;:::,;,'.,ONd.......... . ............'M
+W .                       ...:KNko'..'',,''','',:NO..         ... .           'M
+W..  .                       .'xWX0doldxx0kkkddkWd          ... .     .       .M
+W     .                         oMXx;;,,,;:clxKN:                             .M
+M   ..                          .xWN;....'..'kk.                  ...         'M
+M  .       ...                  ..X0O:.....,Kk..              . .      ...    'M
+M                               ..,Xx:,  .lXXc..            ... .             'M
+M                ....           . 'd0od.'oKOO..                ..  ...        'M
+M                               . ..;dOldl.c'..               ..    .';,....  .M
+M              ...                ...:do,  ....   .            .          ..  .M
+M..         ....                  ...;0:. .....   .          .......          'M
+M       ..                        ...oNll'.''.............     ......         .M
+M           .         .       .. ..'cN;..l::llc::;,,;'......   .  .''.  .     .M
+M                    .  ..    ....'oO:...';...:lldkkOOx;,,;;'....   .....  ....M
+M                  ..   ..    ',;lKx.              ..,lkO00kol;,''.......  ...'M
+M.    .          ...... ..  .'dkOx;...........            'lk0KOo::;,'........'M
 ██╗    ██╗  █████╗  ██╗      ██╗      ███████╗ ████████╗     ██████╗     
 ██║    ██║ ██╔══██╗ ██║      ██║      ██╔════╝ ╚══██╔══╝     ╚════██╗    
 ██║ █╗ ██║ ███████║ ██║      ██║      █████╗      ██║         █████╔╝    
