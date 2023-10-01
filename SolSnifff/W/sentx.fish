67a373a22729be2957b0ae5427e24615ef30e1e4
wezbest <110793713+wezbest@users.noreply.github.com>
2023-10-01 11:33:53 +0000

    .

--- SolSnifff/W/sentx.fish
+++ SolSnifff/W/sentx.fish
@@ -26,27 +26,38 @@ echo (set_color magenta)"//////////////////////////"
 echo ""
 echo "" 
 set SEP https://sepolia.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7
-set W1k = 0x31a07a6705591eb67e83a018eee03e46447d81847149c70972c6eb348ec4190b
+set W1 = 0x76021F7C89C43e7aEf0F58e5A72c582F0A8cf48B
+set W1K = 0x31a07a6705591eb67e83a018eee03e46447d81847149c70972c6eb348ec4190b
+set W2 = 0x7434651d968DbC9CA32268Abbd63EC9Cfb447B31
 set W2K = 0xe97c3c7130b94bc6f64b98d8f1d195ab5e49dea477eed60934d217ce9cd58d27
 echo "Sending 1 Ether from W1 --> W2"
 echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
 cast send \
-    --chain sepolia \
-    --rpc-url $SEP \
-    --private-key $W1K \
-    $W2 \
-    --value 1.00ether
+    --value 0.1ether \
+    0x7434651d968DbC9CA32268Abbd63EC9Cfb447B31 \
+    --rpc-url https://sepolia.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7 \
+    --private-key 0x31a07a6705591eb67e83a018eee03e46447d81847149c70972c6eb348ec4190b
 echo "DONE W1 ---> W2"
+echo "Current Balance - Wallet 1"
+cast balance 0x7434651d968DbC9CA32268Abbd63EC9Cfb447B31 --rpc-url https://sepolia.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo ""
 echo "Sending 1 Ether W1 <-- W2"
 echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
 cast send \
-    --chain sepolia \
-    --rpc-url $SEP \
-    --private-key $W2K \
-    $W1 \
-    --value 1.00ether
+    --value 0.1ether \
+    0x76021F7C89C43e7aEf0F58e5A72c582F0A8cf48B \
+    --rpc-url https://sepolia.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7 \
+    --private-key 0xe97c3c7130b94bc6f64b98d8f1d195ab5e49dea477eed60934d217ce9cd58d27
 echo "DONE W1 <--- W2"
+echo "Current Balance - Wallet 2"
+cast balance 0x76021F7C89C43e7aEf0F58e5A72c582F0A8cf48B --rpc-url https://sepolia.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo ""
