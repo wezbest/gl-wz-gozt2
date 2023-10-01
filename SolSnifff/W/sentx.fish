1460d9bf4582ef4627057db6bc1c56b9685fb0d6
wezbest <110793713+wezbest@users.noreply.github.com>
2023-10-01 10:16:38 +0000

    .

--- SolSnifff/W/sentx.fish
+++ SolSnifff/W/sentx.fish
@@ -23,3 +23,30 @@ echo (set_color magenta)"  --private-key $GOERLI_P_KEY '\'"
 echo (set_color magenta)"  $TW1 '\'"
 echo (set_color magenta)"  --value 0.02ether"
 echo (set_color magenta)"//////////////////////////"
+echo ""
+echo "" 
+set SEP https://sepolia.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7
+set W1k = 0x31a07a6705591eb67e83a018eee03e46447d81847149c70972c6eb348ec4190b
+set W2K = 0xe97c3c7130b94bc6f64b98d8f1d195ab5e49dea477eed60934d217ce9cd58d27
+echo "Sending 1 Ether from W1 --> W2"
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+cast send \
+    --chain sepolia \
+    --rpc-url $SEP \
+    --private-key $W1K \
+    $W2 \
+    --value 1.00ether
+echo "DONE W1 ---> W2"
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo ""
+echo "Sending 1 Ether W1 <-- W2"
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+cast send \
+    --chain sepolia \
+    --rpc-url $SEP \
+    --private-key $W2K \
+    $W1 \
+    --value 1.00ether
+echo "DONE W1 <--- W2"
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo ""
