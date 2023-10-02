58977eec0d3cf595e7a9516196549a6e843a28ec
wezbest <110793713+wezbest@users.noreply.github.com>
2023-10-02 11:29:56 +0000

    .

--- exGO/65_66_67_TypeEmbeddings/ho/ho.go
+++ exGO/65_66_67_TypeEmbeddings/ho/ho.go
@@ -1,5 +1,6 @@
 /*
-My attemtp at excercise
+Type _Embeddings - 
+> Program is for warehouse , and determining automation for item types
 */
 
 package ho
@@ -13,3 +14,21 @@ func Meho() {
 	f.Println("❤️")
 	L.T1("❤️")
 }
+
+// Create constants  - Determines that conveyer belt on which items can move on 
+const (
+	Small = iota // iota automatically assigns a value for next variable
+	Medium
+	Large
+)
+
+// Second constants  - Determines ground and air 
+const (
+	Ground = iota
+	Air
+)
+
+// Types for conveyer belt and shipping type 
+type BeltSize int 
+type Shipping int 
+
