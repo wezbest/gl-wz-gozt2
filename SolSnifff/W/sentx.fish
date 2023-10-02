9339783ea44d2eb98e1bd1dbc001f9bb47f55f6e
wezbest <110793713+wezbest@users.noreply.github.com>
2023-10-02 07:33:07 +0000

    .

--- SolSnifff/W/sentx.fish
+++ SolSnifff/W/sentx.fish
@@ -14,6 +14,8 @@ echo (set_color blue)"W2K = 0xe97c3c7130b94bc6f64b98d8f1d195ab5e49dea477eed60934
 echo (set_color blue)"---------------"
 echo ""
 echo ""
+
+# Showing the command being used
 echo (set_color magenta)"Command Structure "
 echo (set_color magenta)"//////////////////////////"
 echo (set_color magenta)"cast send '\'"
@@ -25,12 +27,25 @@ echo (set_color magenta)"  --value 0.02ether"
 echo (set_color magenta)"//////////////////////////"
 echo ""
 echo "" 
+
+# Defining the variables
 set SEP https://sepolia.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7
+set MUM https://polygon-mumbai.g.alchemy.com/v2/DlxcFxU5u-OioYGMvKhwWsAzAknhvQYd
 set W1 0x76021F7C89C43e7aEf0F58e5A72c582F0A8cf48B
 set W1K 0x31a07a6705591eb67e83a018eee03e46447d81847149c70972c6eb348ec4190b
 set W2 0x7434651d968DbC9CA32268Abbd63EC9Cfb447B31
 set W2K 0xe97c3c7130b94bc6f64b98d8f1d195ab5e49dea477eed60934d217ce9cd58d27
-echo (set_color 97FEED)"Sending 1 Ether from W1 --> W2"
+echo ""
+# Start sending to Sepolia 
+echo (set_color 97FEED)""
+echo " ███████╗ ███████╗ ██████╗   ██████╗  ██╗      ██╗  █████╗ "
+echo " ██╔════╝ ██╔════╝ ██╔══██╗ ██╔═══██╗ ██║      ██║ ██╔══██╗"
+echo " ███████╗ █████╗   ██████╔╝ ██║   ██║ ██║      ██║ ███████║"
+echo " ╚════██║ ██╔══╝   ██╔═══╝  ██║   ██║ ██║      ██║ ██╔══██║"
+echo " ███████║ ███████╗ ██║      ╚██████╔╝ ███████╗ ██║ ██║  ██║"
+echo " ╚══════╝ ╚══════╝ ╚═╝       ╚═════╝  ╚══════╝ ╚═╝ ╚═╝  ╚═╝"
+echo ""
+echo (set_color 97FEED)"Sending 1 Ether from W1 --> W2 --> Sepolia"
 echo "++++++++++++++++++++++++++++++++++++++++++"
 echo "++++++++++++++++++++++++++++++++++++++++++"
 echo "++++++++++++++++++++++++++++++++++++++++++"
@@ -46,7 +61,8 @@ echo "++++++++++++++++++++++++++++++++++++++++++"
 echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo ""
-echo (set_color FBFFB1)"Sending 1 Ether W1 <-- W2"
+echo (set_color 97FEED)"Sending 1 Ether W1 <-- W2 <-- Sepolia"
+echo "++++++++++++++++++++++++++++++++++++++++++"
 echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo "++++++++++++++++++++++++++++++++++++++++++" 
 cast send \
@@ -57,6 +73,48 @@ cast send \
 echo "DONE W1 <--- W2"
 echo "Current Balance - Wallet 2"
 cast balance $W2 --rpc-url $SEP
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo ""
+
+# Start sending to Mumbai
+echo (set_color FBFFB1)""
+echo " ███╗   ███╗ ██╗   ██╗ ███╗   ███╗ ██████╗   █████╗  ██╗"
+echo " ████╗ ████║ ██║   ██║ ████╗ ████║ ██╔══██╗ ██╔══██╗ ██║"
+echo " ██╔████╔██║ ██║   ██║ ██╔████╔██║ ██████╔╝ ███████║ ██║"
+echo " ██║╚██╔╝██║ ██║   ██║ ██║╚██╔╝██║ ██╔══██╗ ██╔══██║ ██║"
+echo " ██║ ╚═╝ ██║ ╚██████╔╝ ██║ ╚═╝ ██║ ██████╔╝ ██║  ██║ ██║"
+echo " ╚═╝     ╚═╝  ╚═════╝  ╚═╝     ╚═╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝"
+echo ""
+echo (set_color FBFFB1)"Sending 1 Ether from W1 --> W2 --> Mumbai"
+echo "++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++"
+cast send \
+    --value 0.1ether \
+    $W2 \
+    --rpc-url $MUM \
+    --private-key $W1K
+echo "DONE W1 ---> W2"
+echo "Current Balance - Wallet 1"
+cast balance $W1 --rpc-url $MUM
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo ""
+echo (set_color FBFFB1)"Sending 1 Ether W1 <-- W2 <-- Mumbai"
+echo "++++++++++++++++++++++++++++++++++++++++++"
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+echo "++++++++++++++++++++++++++++++++++++++++++" 
+cast send \
+    --value 0.1ether \
+    $W1 \
+    --rpc-url $MUM \
+    --private-key $W2K
+echo "DONE W1 <--- W2"
+echo "Current Balance - Wallet 2"
+cast balance $W2 --rpc-url $MUM
 echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo "++++++++++++++++++++++++++++++++++++++++++" 
 echo "++++++++++++++++++++++++++++++++++++++++++" 
